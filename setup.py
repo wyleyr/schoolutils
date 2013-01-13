b45c30839a3f78087039f533c7151b66b06f377a
Richard Lawrence <richard.lawrence@berkeley.edu>
2013-01-12 17:11:15 -0800

    initial directory structure for package distribution with setuptools schoolutils/   schoolutils/     __init__.py     grading/       __init__.py       grader.py       db.py       ui.py   setup.py   project.org   README.rst

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='schoolutils',
+      version='0.1',
+      description='Utilities for teachers, including: a grade database, grade calculators, and more',
+      url='',
+      author='Richard Lawrence',
+      author_email='richard.lawrence@berkeley.edu',
+      license='GPLv2+',
+      packages=['schoolutils'],
+      zip_safe=False)
