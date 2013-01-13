7a361145815a6070fb9163a93915581a1831b845
Richard Lawrence <richard.lawrence@berkeley.edu>
2013-01-13 11:54:44 -0800

    * setup.py: x.x.x version; better description

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
 setup(name='schoolutils',
-      version='0.1',
-      description='Utilities for teachers, including: a grade database, grade calculators, and more',
+      version='0.1.0',
+      description=('Utilities to track and manage student data, including '
+                   'a grade database, grade calculators, and more'),
       url='',
       author='Richard Lawrence',
       author_email='richard.lawrence@berkeley.edu',
       license='GPLv2+',
-      packages=['schoolutils'],)
+      packages=['schoolutils'],
+      #scripts=['bin/grade',],
+      )
