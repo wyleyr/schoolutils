f4e0fcc9a54acd3dc01b8f75ab631289b759a933
Richard Lawrence <richard.lawrence@berkeley.edu>
2013-01-15 10:59:50 -0800

    v0.1.2 release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 from distutils.core import setup
 
 setup(name='schoolutils',
-      version='0.1.1',
+      version='0.1.2',
       description=('Utilities to track and manage student data, including '
                    'a grade database, grade calculators, and more'),
       url='https://bitbucket.org/wyleyr/schoolutils',
