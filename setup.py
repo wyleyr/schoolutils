cbcfa83610bf3e14336c5373333ad52bb3b6b13f
Richard Lawrence <richard.lawrence@berkeley.edu>
2013-01-15 18:15:30 -0800

    v0.1.3 release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ finally:
     readme.close()
     
 setup(name='schoolutils',
-      version='0.1.2',
+      version='0.1.3',
       description=('Utilities to track and manage student data, including '
                    'a grade database, grade calculators, and more'),
       long_description=long_desc,
